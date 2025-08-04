bd31afdb62517a513e85c82df73282d39ce8ebab
yanis <yanis.amine.harrat@gmail.com>
2025-08-04 04:59:20 +0200

    🔄 Réécrit la page d'accueil pour intégrer un nouveau design, incluant une barre de progression et une section de présentation personnelle. Réorganise également les composants et les sections pour améliorer la lisibilité et l'expérience utilisateur, tout en ajoutant des informations sur les technologies utilisées et les compétences en cours d'apprentissage.

--- src/components/floatingDots.tsx
+++ src/components/floatingDots.tsx
@@ -1,4 +1,4 @@
-{/*'use client'
+'use client'
 
 import React, { useState, useEffect } from 'react'
 import { motion } from 'framer-motion'
@@ -79,4 +79,3 @@ const FloatingDots: React.FC = () => {
 }
 
 export default FloatingDots;
-*/}
